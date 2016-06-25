5a17a702d4ac457aa09a84eaf449ad1b71cfde92
HoverHell <hoverhell@gmail.com>
2016-06-25 09:43:54 +0300

    pypiability

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='BeatCop',
       version='1.0',
       description='Cluster singleton manager',
       long_description=open('README.md').read(),
+      author='??',
+      author_email='unspecified@example.com',
+      maintainer='HoverHell',
+      maintainer_email='hoverhell@gmail.com',
       url='https://github.com/Luluvise/BeatCop',
       requires=['redis(>=2.9.1)'],
       scripts=['beatcop.py'],
